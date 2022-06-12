916866eb004cee6db6a7ecf24e3f38d174594b2e
Ivy Kabere <kabereivy@gmail.com>
2022-06-12 22:43:07 +0300

    add Project model

--- main/models.py
+++ main/models.py
@@ -9,3 +9,17 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.bio
+
+class Project(models.Model):
+    title = models.CharField(max_length=155)
+    description = models.TextField(max_length=255)
+    photo = models.ImageField(upload_to='pics/')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="project")
+    link =  models.URLField(max_length=200)
+    design = models.IntegerField(choices=list(zip(range(0,11), range(0,11))), default=0)
+    usability = models.IntegerField(choices=list(zip(range(0,11), range(0,11))), default=0)
+    content = models.IntegerField(choices=list(zip(range(0,11), range(0,11))), default=0)
+    vote_submissions = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.title}'
