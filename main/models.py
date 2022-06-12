65635d3a046bc9465660782ac91d6a270751166e
Ivy Kabere <kabereivy@gmail.com>
2022-06-12 23:19:58 +0300

    make changes to the models file

--- main/models.py
+++ main/models.py
@@ -1,34 +1,48 @@
 from django.db import models
 from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 # Create your models here.
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    profile_picture = models.ImageField(upload_to='images/')
-    bio = models.TextField(max_length=500)
-    contact = models.CharField(max_length=200)
+    avatar = models.ImageField(upload_to='images/', blank=True)
+    bio = HTMLField()
+    contact = HTMLField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
 
-    def __str__(self):
-        return self.bio
-
-class Project(models.Model):
-    title = models.CharField(max_length=155)
-    description = models.TextField(max_length=255)
-    photo = models.ImageField(upload_to='pics/')
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="project")
-    link =  models.URLField(max_length=200)
-    design = models.IntegerField(choices=list(zip(range(0,11), range(0,11))), default=0)
-    usability = models.IntegerField(choices=list(zip(range(0,11), range(0,11))), default=0)
-    content = models.IntegerField(choices=list(zip(range(0,11), range(0,11))), default=0)
-    vote_submissions = models.IntegerField(default=0)
-
-    def __str__(self):
-        return f'{self.title}'
+    def save_profile(self):
+        self.save()
 
 
-class Comment(models.Model):
-    posted_by=models.ForeignKey(User, on_delete=models.CASCADE,null=True)
-    comment_image=models.ForeignKey(Project,on_delete=models.CASCADE,null=True)
-    comment=models.CharField(max_length=20,null=True)
-    
-    def __str__(self):
-        return self.posted_by
+class Post(models.Model):
+    name = models.CharField(max_length=30)
+    image = models.ImageField(upload_to='images/', blank=True)
+    description = HTMLField(blank=True)
+    live_link=models.URLField(blank=True)
+    profile=models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
+    user_profile = models.ForeignKey(User,on_delete=models.CASCADE, related_name='posts',blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+
+
+class Ratings(models.Model):
+    INPUT = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+        (6, '6'),
+        (7, '7'),
+        (8, '8'),
+        (9, '9'),
+        (10, '10'),
+    )
+
+    design=models.IntegerField(choices=INPUT, default=0, blank=True)
+    usability=models.IntegerField(choices=INPUT, blank=True)
+    content=models.IntegerField(choices=INPUT, blank=True)
+    score=models.IntegerField(default=0, blank=True)
+    post_rated = models.ForeignKey(Post,on_delete=models.CASCADE, related_name='ratings',null=True)
+
+    def save_comment(self):
+        self.save()
+
