5294a50ebdd1d717e3006d6d8503362f391af839
Ivy Kabere <kabereivy@gmail.com>
2022-06-13 11:12:17 +0300

    handle media files configurations in settings

--- awards/settings.py
+++ awards/settings.py
@@ -70,6 +70,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -126,6 +127,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+# configuring the location for media
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,8 @@
+from django.conf import settings
 from django.urls import include, path
+from django.conf.urls.static import static
+
+
 from .import views
 
 urlpatterns = [
@@ -6,3 +10,5 @@ urlpatterns = [
     path('new_post/', views.new_post, name='new_post'),
 
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
