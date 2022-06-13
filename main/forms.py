e4f22d0fd4f5359051b7b65d0e05e676051f7c93
Ivy Kabere <kabereivy@gmail.com>
2022-06-13 11:27:22 +0300

    add signup form and functions

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,15 @@
 from django import forms
 from .models import *
+from django.contrib.auth.forms import UserCreationForm
 
 class UploadForm(forms.ModelForm):
     class Meta:
         model = Post
         exclude = ['user_profile', 'profile']
+
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
--- main/views.py
+++ main/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth import login, authenticate
 
 from .models import *
 from . forms import *
 
 # Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignupForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
+
 def index(request):
    posts = Post.objects.all()
    current_user = request.user
