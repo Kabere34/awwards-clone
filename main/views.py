2cd96afda1fef3dc62926ddaf41b0e358aaeb109
Ivy Kabere <kabereivy@gmail.com>
2022-06-13 10:29:17 +0300

    fetch Post fields on in dex page

--- main/views.py
+++ main/views.py
@@ -5,14 +5,12 @@ from . forms import *
 
 # Create your views here.
 def index(request):
-   # posts = Post.all_posts()
+   posts = Post.objects.all()
    current_user = request.user
-
-   # context ={
-
-   # "posts":posts ,
-
-   return render(request, 'main/index.html')
+   context ={
+   "posts":posts ,
+   }
+   return render(request, 'main/index.html', context)
 
 def new_post(request):
    current_user=request.user
