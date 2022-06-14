67544663bd4cc5251d154828654c48348f59886e
Ivy Kabere <kabereivy@gmail.com>
2022-06-14 16:00:10 +0300

    add navbar

--- main/urls.py
+++ main/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('account/', include('django.contrib.auth.urls')),
     path('profile/', views.profile, name='profile'),
     path('user_profile/<int:user_id>', views.user_profile, name='user_profile'),
-    path('profile_edit/<int:user_id>', views.profile_edit, name='profile-edit'),
+    path('profile_edit/<int:user_id>', views.profile_edit, name='profile_edit'),
     # path('rate/(?P<pk>\d+)', views.rate_post, name='rate_post'),
     path('api/posts/', views.Postlist.as_view()),
 
