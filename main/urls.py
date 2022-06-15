7d7e0a66cb4ee2a46e8bf81ba1ebc5739e0bd685
Ivy Kabere <kabereivy@gmail.com>
2022-06-15 03:37:13 +0300

    add url path for single_post template

--- main/urls.py
+++ main/urls.py
@@ -9,13 +9,14 @@ urlpatterns = [
 
     path('', views.index, name='index'),
     path('new_post/', views.new_post, name='new_post'),
+    path('single_post/<int:post_id>', views.single_post, name='single_post'),
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
     path('account/', include('django.contrib.auth.urls')),
     path('profile/', views.profile, name='profile'),
     path('user_profile/<int:user_id>', views.user_profile, name='user_profile'),
     path('profile_edit/<int:user_id>', views.profile_edit, name='profile_edit'),
-    # path('rate/(?P<pk>\d+)', views.rate_post, name='rate_post'),
+    path('rate/<pk>', views.rate_post, name='rate_post'),
     path('api/posts/', views.Postlist.as_view()),
 
 ]
